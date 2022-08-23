483c9f1dfbebf50119a7662dc3670c3ccf4c043a
Douglas_NotDell <douglas.santos106@yahoo.com>
2022-08-23 18:39:56 -0300

    Conta aws spam - partner-led

--- terraform-payer-ecam-partner-led/policis_organiz.tf
+++ terraform-payer-ecam-partner-led/policis_organiz.tf
@@ -7,11 +7,7 @@
 resource "aws_organizations_policy" "policy_billing" {
   name        = "Compass-deny-billing"
   description = "Policy que bloqueia o acesso ao billing pelo cliente."
-  tags = {
-    Environment = "Payer-ECAM-Partnet-led"
-    ManagedBy   = "Terraform"
-    Owner       = "Compass-cloud-n1"
-  }
+  tags = var.registro
 
   content = <<CONTENT
 {
@@ -47,11 +43,7 @@ CONTENT
 resource "aws_organizations_policy" "policy_supportDeny" {
   name        = "SCPCompassoUOL-SupportDeny"
   description = "SCP Compasso UOL - Support Deny"
-  tags = {
-    Environment = "Payer-ECAM-Partnet-led"
-    ManagedBy   = "Terraform"
-    Owner       = "Compass-cloud-n1"
-  }
+  tags = var.registro
 
   content = <<CONTENT
 {
@@ -88,11 +80,7 @@ CONTENT
 resource "aws_organizations_policy" "policy_ProtectAWSSSO" {
   name        = "SCPCompassoUOL-ProtectAWSSSO"
   description = "SCP Compasso UOL - Protect Settings on AWS SSO"
-  tags = {
-    Environment = "Payer-ECAM-Partnet-led"
-    ManagedBy   = "Terraform"
-    Owner       = "Compass-cloud-n1"
-  }
+  tags = var.registro
 
   content = <<CONTENT
 {
@@ -111,66 +99,6 @@ resource "aws_organizations_policy" "policy_ProtectAWSSSO" {
           "aws:PrincipalArn": [
             "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_CompassoCloudTeam_*",
             "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_CompassoAdmins_*"
-          ]
-        }
-      }
-    }
-  ]
-}
-CONTENT
-}
-
-# SCPCompassoUOL-Lendico-DenyAccessByip
-#------------------------------------------------
-
-resource "aws_organizations_policy" "policy_DenyAccessByip" {
-  name        = "SCPCompassoUOL-Lendico-DenyAccessByip"
-  description = "Permite o acesso para a conta de apenas os ips do cliente Lendico"
-  tags = {
-    Environment = "Payer-ECAM-Partnet-led"
-    ManagedBy   = "Terraform"
-    Owner       = "Compass-cloud-n1"
-  }
-
-  content = <<CONTENT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Statement2",
-      "Effect": "Deny",
-      "Action": [
-        "*"
-      ],
-      "Resource": [
-        "*"
-      ],
-      "Condition": {
-        "NotIpAddress": {
-          "aws:SourceIp": [
-            "179.191.73.115/32",
-            "177.69.75.25/32"
-          ]
-        },
-        "ArnNotEquals": {
-          "aws:PrincipalArn": [
-            "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_CompassoSuporte_*",
-            "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_CompassoCloudTeam_*",
-            "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_CompassoAdmins_*",
-            "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_CompassoNetwork_*",
-            "arn:aws:iam::*:role/uoldiveo-mgmt-cost",
-            "arn:aws:iam::*:role/CrossAccountAdmin"
-          ]
-        },
-        "ForAnyValue:StringNotLike": {
-          "aws:SourceVpc": [
-            "vpc-0617b9535b7b831cf",
-            "vpc-061b887fb9eaa59f2",
-            "vpc-0eff3a09bc98b6d92",
-            "vpc-508e0136",
-            "vpc-5f6e603a",
-            "vpc-8547b2fe",
-            "vpc-a273eac7"
           ]
         }
       }
